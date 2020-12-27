74dd15a144d84b550255b9b950a838bd08175270
JunYuCoding <62270942+JunYuCoding@users.noreply.github.com>
2020-12-27 17:59:11 +0800

    Add files via upload

---
+++ blog/js/footer.js
@@ -0,0 +1,4 @@
+document.write('	<div class="container">																')
+document.write('			<p>Copyright @ 2020-12 <a href="">JUNU | 理想的生活就是生活的理想</a>· powered by hjy </p>														')
+document.write('			  <a href="mailto:#">Contact with me</a>														')
+document.write('			</div>														')
